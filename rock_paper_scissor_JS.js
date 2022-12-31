12be91c262ab6fcc53619243607b168193c5da4a
Akhil <pletiakhil100@gmail.com>
2022-12-31 12:33:21 +0530

    Add UI

--- rock_paper_scissor_JS.js
+++ rock_paper_scissor_JS.js
@@ -1,82 +1,67 @@
-function getComputerChoice() {
-    let computerChoice = Math.floor(Math.random() * 10 + 1);
-    // console.log(computerChoice);
-    if (computerChoice < 3) {
-        return "rock";
-    }
-    else if (computerChoice < 6) {
-        return "paper";
-    }
-    else {
-        return "scissors";
-    }
-}
-function validatePlayerChoice(playerChoice) {
-    playerChoice = playerChoice.toLowerCase();
-    if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors") {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
-function getPlayerChoice() {
-    let playerChoice = prompt("Choose Rock Paper OR Scissors - ");
-    if (validatePlayerChoice(playerChoice)) {
-        return playerChoice.toLowerCase();
-    }
-    else {
-        alert("Choose only rock paper or scissors!");
-        getPlayerChoice();
-    }
+let playerScore = 0;
+let computerScore = 0;
+let round = 0;
+let msg = 'Can you beat the computer?';
+
+function updateInfo(msg,playerScore,computerScore,round){
+    document.getElementById('msg').textContent = msg;
+    document.getElementById('player-score').textContent = `player score - ${playerScore}`;
+    document.getElementById('computer-score').textContent = `computer score - ${computerScore}`;
+    document.getElementById('round').textContent = `round - ${round}`;
 }
 
-function game(playerChoice, computerChoice) {
+function getComputerChoice(){
+    let rps = ['rock','paper','scissor'];
+    let random = rps[Math.floor(Math.random()*rps.length)];
+    return random;
+};
+
+function game(playerChoice){
+    let computerChoice = getComputerChoice();
     let outcome = 0;
+
+    if (playerScore === 5 || computerScore === 5){
+        if(playerScore === 5){
+            msg = "You Win";
+        }
+        else{
+            msg = "You Lose";
+        }
+        updateInfo(msg,playerScore,computerScore,round);
+        return 0;
+    }
     // DRAW
     if (playerChoice === computerChoice) {
-        alert("The game is a DRAW!! You both choose " + playerChoice + "!");
+        msg = ("The game is a DRAW!! You both choose " + playerChoice + "!");
     }
     // Player WIN
-    else if ((playerChoice === "rock" && computerChoice === "scissors") || (playerChoice === "paper" && computerChoice === "rock") || (playerChoice === "scissors" && computerChoice === "paper")) {
-        alert("You WIN!! You choose " + playerChoice + " and Computer chose " + computerChoice + ".");
-        outcome = 1;
+    else if ((playerChoice === "rock" && computerChoice === "scissor") || (playerChoice === "paper" && computerChoice === "rock") || (playerChoice === "scissor" && computerChoice === "paper")) {
+        msg = ("You WIN!! You choose " + playerChoice + " and Computer chose " + computerChoice + ".");
+        playerScore++;
     }
     // PLAYER LOSE
     else {
-        alert("You LOSE!! You choose " + playerChoice + " and Computer chose " + computerChoice + ".");
+        msg = ("You LOSE!! You choose " + playerChoice + " and Computer chose " + computerChoice + ".");
         outcome = -1
-    }
-    return outcome;
-}
-
-let computerScore = 0;
-let playerScore = 0;
-for (let i = 0; i < 5; i++) {
-    let playerChoice = getPlayerChoice();
-    let computerChoice = getComputerChoice();
-    if (playerChoice === null) {
-        console.log("Ok");
-    }
-    let score = game(playerChoice, computerChoice);
-    if (score === 1) {
-        playerScore++;
-    }
-    else if (score === -1) {
         computerScore++;
     }
-    console.group("SCOREBOARD");
-    console.log("player score - " + playerScore);
-    console.log("computer score - " + computerScore);
-    console.groupEnd();
-}
+    round++;
+    updateInfo(msg,playerScore,computerScore,round);
 
-if (playerScore > computerScore) {
-    console.log("YOU WIN!!!");
-}
-else if (playerScore < computerScore) {
-    console.log("YOU LOSE!!!");
-}
-else {
-    console.log("IT IS A DRAW -_-");
-}
+};
+
+document.getElementById('rock').addEventListener('click',()=>{
+    game('rock');
+});
+document.getElementById('paper').addEventListener('click',()=>{
+    game('paper');
+});
+document.getElementById('scissor').addEventListener('click',()=>{
+    game('scissor');
+});
+
+document.getElementById('new-game').addEventListener('click',()=>{
+    updateInfo('Can you beat the computer?',0,0,0);
+
+});
+
